0a627fc1c281f0c7bef3180c65af567b3b405a7d
yuanzhirong <283211086@qq.com>
2023-04-14 17:34:27 +0800

    通过child_process  启动所有子项目

---
+++ build/run.js
@@ -0,0 +1,30 @@
+/*
+ * Author: yuanzhirong
+ * Date: 2023-04-14 17:07:03
+ * LastEditors: yuanzhirong
+ * LastEditTime: 2023-04-14 17:12:05
+ * Description:
+ */
+const path = require("path");
+const childProcess = require("child_process");
+
+const filePath = {
+  vue2: path.join(__dirname, "../vue2"),
+  vue3: path.join(__dirname, "../vue3"),
+  react15: path.join(__dirname, "../react15"),
+  react16: path.join(__dirname, "../react16"),
+};
+
+//cd 子应用目录  npm start 启动项目
+function runChild() {
+  Object.keys(filePath).forEach((item) => {
+    const childPath = filePath[item];
+
+    childProcess.spawn(`cd ${childPath} && npm start`, {
+      stdio: "inherit",
+      shell: true,
+    });
+  });
+}
+
+runChild();
